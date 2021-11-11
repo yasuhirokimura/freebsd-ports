e198c1cf2310a5f7384675fa741c9639e6e8c1e6
Yasuhiro Kimura <yasu@FreeBSD.org>
2021-11-11 17:42:27 +0900

    Mk/Scripts/sed_checked.sh: Make it respect REINPLACE_ARGS
    Currently this script always uses '.bak' as extension of backup files.
    As a result REINPLACE_CMD doesn't work as is expected with developer
    mode if REINPLACE_ARGS is set differently from default value
    (e.g. "-i ''"). So make the script respect REINPLACE_ARGS.

--- Mk/Scripts/sed_checked.sh
+++ Mk/Scripts/sed_checked.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 
 set -e
-/usr/bin/sed -i.bak "$@"
+if [ $1 = "-i" ]; then
+    original_extension="$2"
+    shift
+    shift
+else
+    original_extension="$(echo $1 | sed -n 's/^-i\(.*\)/\1/p')"
+    if [ -z "${original_extension}" ]; then
+	echo Invalid first argument: "$1" 1>&2
+	exit 1
+    fi
+    shift
+fi
+if [ -z "${original_extension}" ]; then
+    extension=.sed_checked
+else
+    extension="${original_extension}"
+fi
+sed -i"${extension}" "$@"
 for x in "${@}" ; do
     if [ -f "${x}" ]; then
-	if cmp -s "${x}" "${x}".bak ; then
-            if [ ! -z "${REWARNFILE}" ]; then
-                echo - - REINPLACE_CMD ran, but did not modify file contents: ${x#${WRKSRC}/} >> ${REWARNFILE}
-            fi
-        fi
+	if cmp -s "${x}" "${x}${extension}" ; then
+	    if [ ! -z "${REWARNFILE}" ]; then
+		echo - - REINPLACE_CMD ran, but did not modify file contents: ${x#${WRKSRC}/} >> ${REWARNFILE}
+	    fi
+	fi
+	if [ -z "${original_extension}" ]; then
+	    rm -f "${x}${extension}"
+	fi
     fi
 done
